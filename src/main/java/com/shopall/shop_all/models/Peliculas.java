507cadf80227eeef8f1b841b0095830947eeec7b
AleBC19 <celestinobautistaalejandra@gmail.com>
2025-06-28 00:38:22 -0600

    modelo Peliculas

--- src/main/java/com/shopall/shop_all/models/Peliculas.java
+++ src/main/java/com/shopall/shop_all/models/Peliculas.java
@@ -1,5 +1,31 @@
 package com.shopall.shop_all.models;
 
-public class Peliculas {
+import java.util.ArrayList;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Peliculas {
+  @Id
+      @GeneratedValue(strategy = GenerationType.AUTO)
+      private Long id;
+  
+  @Column
+    private String nombre;
+
+  @Column 
+    private String disponible;
 }
